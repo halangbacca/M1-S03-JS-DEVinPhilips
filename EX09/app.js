d2f35d7e1dd71d7358e48c1a86400ba3c875ae37
Halan <halanbacca@outlook.com>
2022-10-23 21:11:43 -0300

    Exercícios realizados

---
+++ EX09/app.js
@@ -0,0 +1,16 @@
+/*Crie uma função que receba como parâmetro um objeto contendo 2 atributos,
+texto e termo, como o exemplo e teste se o conteúdo de texto contém o conteúdo do termo.*/
+
+const objeto = {
+  texto: "O rato roeu a roupa do rei de Roma.",
+  termo: "rato",
+};
+
+function procuraTermo(objeto) {
+  /*destruct
+  const { texto, termo } = objeto;
+  return texto.includes(termo);*/
+  return objeto.texto.includes(objeto.termo);
+}
+
+console.log(procuraTermo(objeto));
